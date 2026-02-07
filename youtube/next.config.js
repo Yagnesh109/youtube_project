f98b0164e5a65eb14181c139b7b8bb1704bdefb1
Yagnesh <kotwalyagnesh10@gmail.com>
2026-02-08 00:18:54 +0530

    next config file change

--- youtube/next.config.js
+++ youtube/next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_BACKEND_URL: process.env.BACKEND_URL || "http://localhost:5000",
+    NEXT_PUBLIC_BACKEND_URL:
+      process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:5000",
   },
 };
 
